db11815355fdb1e4faf3fb4dda5d2bf24bb9141a
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2023-03-05 11:01:56 +0900

    first commit

---
+++ app/activateScraper/route.ts
@@ -0,0 +1,12 @@
+export async function POST(request: Request) {
+
+
+}
+
+export async function GET(request: Request) {
+
+  return new Response('Hello, PAPAFAM!!!!', {
+    status: 200,
+
+  });
+}
